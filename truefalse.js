cbce6329e07e2742f7152d8bf4bec28261c03fef
towhiid <towhid3008@gmail.com>
2020-08-15 02:21:40 +0600

    learning

---
+++ truefalse.js
@@ -0,0 +1,24 @@
+// const age = 5;
+// if(age){
+// console.log("age is true");
+// }else{
+//     console.log("age is false");
+// }
+// const age = 0;
+// if(age){
+// console.log("age is true");
+// }else{
+//     console.log("age is false");
+// }
+// const name = "Atikul"; truthy
+// if(name.length > 0){
+// console.log("age is true");
+// }else{
+//     console.log("age is false");
+// }
+const name = ""; //falsy, space between string would be true.
+if(name){
+console.log("age is true");
+}else{
+    console.log("age is false");
+}
